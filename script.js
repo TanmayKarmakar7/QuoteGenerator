369eee1a7dc395d7407c78adc973b18646de1147
Tanmay Karmakar <tanmaykarmakar746@gmail.com>
2024-10-26 08:00:41 +0530

    some changes done

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const api_url = "https://api.quotable.io/random";
 const quote = document.querySelector(".quote");
 const author = document.querySelector(".author");
 
-async function getQuote(url){
-    const response = await fetch(url);
-    var data = await response.json();
+async function getQuote(){
+    let response = await fetch('https://api.quotable.io/random');
+    let data = await response.json();
     console.log(data);
     quote.innerHTML = data.content;
     author.innerHTML = `~ ${data.author}`;
